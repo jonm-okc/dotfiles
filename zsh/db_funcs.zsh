e90341b2691e1052a446ecf307b6ec3e1aca3c5f
Jon M <jonm@okcupidlabs.com>
2013-04-27 09:18:31 -0700

    tweaks to zsh config

--- zsh/db_funcs.zsh
+++ zsh/db_funcs.zsh
@@ -60,13 +60,19 @@ function sq-day() {
   echo "DATE_FORMAT($col, '%Y-%m-%d') as ${col}_day"
 }
 
+# Outputs SQL queries that can be used to get a count of distributions in a
+# table, grouped by the columns specified.  If the column starts with "mon_"
+# or "day_", it transforms a DATE or DATETIME columns into a month or date.
+#
+# Ex: sql-distro users mon_created_at status
+# => SELECT DATE_FORMAT(created_at, '%Y-%m') as mon_created_at, status, FORMAT(COUNT(*),0) as num_rows FROM users GROUP BY mon_created_at, status;
 function sql-distro() {
   if (( $# < 2 )) ; then
     echo "Usage: sql-distro table col1 [col2...]"
     return -1
   fi
 
-# For some reason, using arrays here causes zsh to crash.  awesome
+  # For some reason, using arrays here causes zsh to crash.  awesome
   # local group_by_arr=()
   # local columns_arr=()
 
@@ -89,9 +95,6 @@ function sql-distro() {
     columns="${columns}, $col"
   done
   
-#  IFS=', '
-#  local sql="SELECT ${columns[*]}, COUNT(*) as num_rows FROM $table GROUP BY ${group_by[*]};"
-
   local sql="SELECT ${columns:2}, FORMAT(COUNT(*),0) as num_rows FROM $table GROUP BY ${group_by:2};"
   echo $sql | pbcopy
   echo $sql
